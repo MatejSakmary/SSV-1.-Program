06ee857b2136371c1cd0e50c86de3af80bb8db48
Jan_Hybs <jan.hybs@tul.cz>
2016-09-21 15:57:52 +0200

    Add draw holder for class Draw

---
+++ src/DrawHolder.java
@@ -0,0 +1,34 @@
+
+import java.awt.Color;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jan-hybs
+ */
+public class DrawHolder extends javax.swing.JPanel {
+
+    /**
+     * Creates new form DrawHolder
+     */
+    public DrawHolder() {
+        initComponents();
+        this.setBackground(Color.BLACK);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setLayout(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
